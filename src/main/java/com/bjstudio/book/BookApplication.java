17d1da25240caef865befb19b6d8f38581e8c0e3
BJStudio25 <bjstudio.it@gmail.com>
2020-04-13 11:47:51 +0200

    Fixed wrong pakage and Added Eureka config.

--- src/main/java/com/bjstudio/book/BookApplication.java
+++ src/main/java/com/bjstudio/book/BookApplication.java
@@ -2,12 +2,14 @@ package com.bjstudio.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class DemoApplication {
+public class BookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BookApplication.class, args);
 	}
 
 }
